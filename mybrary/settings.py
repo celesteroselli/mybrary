a446dc5ff8a18a03efa3a957b42b55c3ae7699ef
Celeste Roselli <celesteroselli@Celestes-MBP.attlocal.net>
2022-12-23 10:46:41 -0500

    changed requirements.txt to remove pyobjc

--- mybrary/settings.py
+++ mybrary/settings.py
@@ -143,8 +143,6 @@ EMAIL_HOST_PASSWORD = 'pxyvbcvyvzwvhdjb'
 # Custom setting. To email
 RECIPIENT_ADDRESS = 'cactuscat.software@gmail.com'
 
-# Heroku: Update database configuration from $DATABASE_URL.
-
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
